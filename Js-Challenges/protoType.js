9911176ae74381bbafd58b892ffcfaa2b9c1c5ad
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2025-03-06 19:46:13 +0530

    working on proto problem 5

--- Js-Challenges/protoType.js
+++ Js-Challenges/protoType.js
@@ -13,3 +13,19 @@ Playlist.prototype.addSong = function (songObj){
 
 let playList = new Playlist()
 console.log("addSong result-",playList.addSong({"song":"to hai to main hu"}))
+// You need to implement the ShoppingCart constructor function and its prototype methods
+
+function ShoppingCart() {
+    // Initialize items property
+    this.items = []
+  }
+  
+  // Define addItem method on ShoppingCart's prototype
+  ShoppingCart.prototype.addItem = function (price) {
+    this.items.push(price);
+  }
+  
+  // Define getTotalPrice method on ShoppingCart's prototype
+  ShoppingCart.prototype.getTotalPrice = function () {
+    return this.items.reduce((acc, item) => acc + item, 0)
+  }
