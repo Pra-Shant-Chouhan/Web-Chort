8b6d5273802510900b80587edd0648a7de88eb6c
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2025-03-06 19:16:10 +0530

    folder structure and rectify code.

--- Js-Challenges/protoType.js
+++ Js-Challenges/protoType.js
@@ -7,5 +7,9 @@ function Playlist() {
 }
 
 Playlist.prototype.addSong = function (songObj){
- return this.songs.push.songObj.song;
+  this.songs.push.songObj;
+  return songObj.song;
 }
+
+let playList = new Playlist()
+console.log("addSong result-",playList.addSong({"song":"to hai to main hu"}))
